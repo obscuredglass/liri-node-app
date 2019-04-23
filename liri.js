96fb3085343e9f69af1eb5a58bec82ae0e45ddbe
ahochman <ahochman2019@gmail.com>
2019-04-22 20:01:24 -0400

    some spotify info

--- liri.js
+++ liri.js
@@ -2,8 +2,8 @@
 
 require("dotenv").config();
 
-const Spotify = require("node-spotify-api");
-const Spotify = new Spotify(keys.Spotify);
+var Spotify = require("node-spotify-api");
+var Spotify = new Spotify(keys.Spotify);
 const keys = require("./keys.js");
 const moment = require("moment");
 const fs = require("fs");
@@ -15,25 +15,25 @@ let bandsintown = (keys.bandsintown);
 var userInput = process.argv[2];
 var userQuery = process.argv.slice[3].join(" ");
 
-//func
+// switch func layout
 
 function userCommand(userInput, userQuery) {
 
 switch (userInput) {
 case "concert-this":
-  concertthis();
+  concertThis(userQuery);
   break;
 
 case "spotify-this-song":
-spotifythissong();
+spotifyThisSong(userQuery);
   break;
 
 case "movie-this":
-  moviethis();
+  movieThis(userQuery);
   break;
 
 case "do-what-it-says":
-  dowhatitsays(userQuery);
+  doWhatItSays(userQuery);
   break;
 
   default:
@@ -42,11 +42,59 @@ case "do-what-it-says":
 }
 }
 
-function concertInfo(userQuery){
+
+//concert this
+
+function concertThis(userQuery){
+  var artist = 
   var queryUrl = "https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=codingbootcamp";
   request(queryUrl, function(error, response, body){
+  axios.get(queryUrl).then(
+  function(reponse) {
 
+console.log("===============");
+console.log("Name of the venue"  + response.data[0].venue.name + "\r\n");
+console.log("Venue location: " + reponse.data[0].venue.city + "\r\n");
+console.log("Date of event: ") + moment(reponse.data[0].datetime).formate("MM-DD-YYYY");
+
+var concertLog = "====Concert info=====" + "\nName of the band" + artist
+
+
+
+
+  }
+
+
+
+)
   };
+
+
+function spotifyThisSong(songName), function(err, data) {
+  if (err) {
+    return console.log("Error occured");
+  }
+  spotify.search({type: "track", query: songName}
+  
+  console.log("========");
+  console.log("Artist Name: " + data.track.items[0].album.artists[0].name + "\r\n");
+  console.log("Song Name: " + data.tracks.items[0].name + "\r\n");
+  console.log("Track preview link: " + data.tracks[0].href + "\r\n");
+  console.log("Album name: " + data.tracks.items[0].album.name + "\r\n");
+  
+  
+  
+  
+  )
+
+
+  
+
+
+
+
+}
+
 
 
 
@@ -62,7 +110,7 @@ function moviethis() {
       "Title: " + movieData.Title + "\n" +
       "Year: " + movieData.Year + "\n" +
       "IMDB Ration: " + movieData.Ratings[0].Value + 
-      "\n"
+      "\n" +
       "Rotten Tomatoes Rating: " + movieData.Rating[1].Value + "\n" +
       "Origin COuntry: " + movieData.Country + "\n" +
       "Plot: " + movieData.Plot + "\n" +
@@ -73,18 +121,9 @@ function moviethis() {
     }
 
 
-
-
   }
   
-  
-  
-  
   )
-
-
-
-
 
 
 
